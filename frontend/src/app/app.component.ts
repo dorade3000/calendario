e6b4b76904397952d84db45e17c0a49641dcfb4e
Torben Kunow <kunow@subshell.com>
2026-05-24 21:21:26 -0400

    angular material installed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {LoginPageComponent} from "./features/login-page/login-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    LoginPageComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/app/features/login-page/login-page.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-login-page',
+  imports: [
+    MatButton
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+
+}
