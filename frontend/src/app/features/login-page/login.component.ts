7b66005215788e2fff67053ddb66e041d6606061
Torben Kunow <kunow@subshell.com>
2026-05-24 22:21:15 -0400

    added login button and title for login screen

--- frontend/src/app/features/login-page/login.component.ts
+++ frontend/src/app/features/login-page/login.component.ts
@@ -1,12 +1,15 @@
 import {Component} from '@angular/core';
 import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
+import {MatButton, MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-login-page',
   imports: [
     MatFormField,
     MatInput,
-    MatLabel
+    MatLabel,
+    MatButton,
+    MatButtonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
