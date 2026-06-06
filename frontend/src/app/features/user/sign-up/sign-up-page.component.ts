bd0195638eaa31a2178bb60f6180b1b348ccedd2
Torben Kunow <kunow@subshell.com>
2026-06-05 20:18:48 -0400

    2 mat-form-felder + button zum registrieren

--- frontend/src/app/features/user/sign-up/sign-up-page.component.ts
+++ frontend/src/app/features/user/sign-up/sign-up-page.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatLabel,
+
+  ],
   templateUrl: './sign-up-page.component.html',
   styleUrl: './sign-up-page.component.scss'
 })
